45de24907270e1ca722b0b139485a6853a0ad273
Paul Tonning <paul.tonning@student.vvc.edu>
2020-10-14 16:04:05 -0700

    update

--- basic_assembly_language/control_structures/while_loop/asm_main.asm
+++ basic_assembly_language/control_structures/while_loop/asm_main.asm
@@ -13,7 +13,7 @@ segment .data
 ;
 ; Output strings
 ;
-
+space db " ",0
 yes_string db    "Yes",10, 0
 no_string db    "No",10, 0
 end_string db    "End",10, 0
@@ -30,21 +30,23 @@ asm_main:
         pusha
 
 
-b1:
 	mov eax,10
 	mov ebx,20
+    ; while (eax < ebx)
 
-while:
+__while__01:
 	cmp eax,ebx
-	jae endwhile
-	mov [tempvalue],eax
+	jae __endwhile__01
+	;mov [tempvalue],eax
+    call print_int
 	mov ecx,eax		; cache the value of eax
+    call print_string
 	mov eax,yes_string
 	call print_string
 	mov eax,ecx
 	inc eax
-	jmp while
-endwhile:
+	jmp __while__01
+__endwhile__01:
 
 	mov eax,end_string
 	call print_string
